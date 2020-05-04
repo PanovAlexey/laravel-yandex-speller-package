ec6945277061f9efd2ef7d8f9b8a9baf0ebb4788
Panov Alexey <alexeykapanov@gmail.com>
2020-05-04 08:02:33 +0300

    Publish package config file.

---
+++ config/yandexspeller.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'yandex_speller_api_url' => 'http://speller.yandex.net/services/spellservice.json/checkTexts',
+    'yandex_speller_curlopt_timeout' => 10,
+    'yandex_speller_curlopt_connecttimeout' => 10
+];
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,13 +1,21 @@
 <?php
 
-
 namespace Laravel\YandexSpeller;
-
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
+    public function register()
+    {
+        parent::register();
+    }
+
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/../routes/yandex-speller.php');
+        $this->publishes([
+            __DIR__ . '/../config/yandexspeller.php' => config_path('yandexspeller.php'),
+            'yandexspeller-config'
+        ]);
+        $this->mergeConfigFrom(__DIR__ . '/../config/yandexspeller.php', 'yandexspeller');
     }
  }
