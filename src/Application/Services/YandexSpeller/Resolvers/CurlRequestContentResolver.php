c011acf2c4f22904ea899f62fba45f9bf1df4f08
Panov Alexey <alexeykapanov@gmail.com>
2020-05-04 08:15:40 +0300

    Create a universal wrapper for CURL queries.

---
+++ src/Application/Services/YandexSpeller/Resolvers/CurlRequestContentResolver.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Laravel\YandexSpeller\Application\Services\YandexSpeller\Resolvers;
+
+class CurlRequestContentResolver
+{
+    /**
+     * @param string $url
+     * @param array $postData
+     * @param array $optionsList
+     * @return array
+     */
+    function handle(string $url, array $postData = [], array $optionsList = [])
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_URL, $url);
+
+        if (!empty($postData)) {
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($postData));
+        }
+
+        if (!empty($optionsList)) {
+            foreach ($optionsList as $optionKey => $optionValue) {
+                curl_setopt($curl, $optionKey, $optionValue);
+            }
+        }
+
+        $result = [
+            'result' => curl_exec($curl),
+            'errno' => curl_errno($curl),
+            'error' => curl_error($curl),
+            'http_code' => curl_getinfo($curl, CURLINFO_HTTP_CODE),
+        ];
+
+        curl_close($curl);
+
+        return $result;
+    }
+}
