1fff74d51cd0ab4107506a5abe5fabf1681a2310
Panov Alexey <alexeykapanov@gmail.com>
2020-05-04 08:26:25 +0300

    Create a controller and add routes for package.

--- routes/yandex-speller.php
+++ routes/yandex-speller.php
@@ -1,6 +1,10 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use Laravel\YandexSpeller\Controller\YandexSpellerController;
 
-Route::get('/yandex-speller', function () {
-    return 'it works';
+Route::prefix('api')->middleware('api')->group(function() {
+    Route::get('/yandex-speller/{sourceString?}', [YandexSpellerController::class, 'index']);
+    Route::match(['post', 'put', 'patch', 'delete', 'options'], '/yandex-speller/{sourceString?}', [
+        YandexSpellerController::class, 'incorrectMethod']
+    );
 });
---
+++ src/Controller/YandexSpellerController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Laravel\YandexSpeller\Controller;
+
+use Illuminate\Routing\Controller;
+use Laravel\YandexSpeller\Application\Services\YandexSpeller\YandexSpellerService;
+
+class YandexSpellerController extends Controller
+{
+    public function index(?string $sourceString = '') {
+        if (!empty($sourceString)) {
+            try {
+                $yandexSpellerService = resolve(YandexSpellerService::class);
+                $yandexSpellerAnswer = $yandexSpellerService->getAnswerByString($sourceString);
+            } catch (\Throwable $throwable) {
+                $yandexSpellerAnswer = null;
+            }
+
+            if (empty($yandexSpellerAnswer)) {
+                $result = [
+                    'status' => 500,
+                    'body' => ['error' => ['message' => 'Internal error. Please, try again later']]
+                ];
+            } else {
+                $result = [
+                    'status' => 200,
+                    'body' => [
+                        'source_string' => $yandexSpellerAnswer->getSourceString(),
+                        'corrected_array' => $yandexSpellerAnswer->getCorrectedArray(),
+                    ]
+                ];
+            }
+        } else {
+            $result = [
+                'status' => 400,
+                'body' => ['error' => ['message' => 'Invalid string']]
+            ];
+        }
+
+        return response()->json($result['body'], $result['status']);
+    }
+
+    public function incorrectMethod() {
+        $result = [
+            'status' => 405,
+            'body' => ['error' => ['message' => 'Method Not Allowed']]
+        ];
+
+        return response()->json($result['body'], $result['status']);
+    }
+}
